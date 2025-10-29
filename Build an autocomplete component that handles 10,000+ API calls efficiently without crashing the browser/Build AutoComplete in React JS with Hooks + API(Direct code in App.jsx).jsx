f7d288950a7c0dcaa2cd4b38b29733007733d25a
Ritika Gaur <150791025+GaurRitika@users.noreply.github.com>
2025-10-29 19:25:44 +0530

    Build autocomplete component for country search
    Implemented an autocomplete component that fetches and filters country data based on user input. Utilized React hooks for state management and API calls.

--- Build an autocomplete component that handles 10,000+ API calls efficiently without crashing the browser/Build AutoComplete in React JS with Hooks + API(Direct code in App.jsx).jsx
+++ Build an autocomplete component that handles 10,000+ API calls efficiently without crashing the browser/Build AutoComplete in React JS with Hooks + API(Direct code in App.jsx).jsx
@@ -1,1 +1,45 @@
+import React , {useState , useEffect} from "react";
+import "./App.css";
+import axios from "axios";
 
+function App() {
+  const [countries ,setCountries] = useState([]);
+  const [countryMatch , setCountryMatch] = useState([]);
+
+  //let's use hooks here , useEffect
+  useEffect(() => {
+    const loadCountries = async () => {
+      const response = await axios.get("https://restcountries.com/v3.1/independent?status=true");
+      setCountries(response.data);
+      };
+
+    loadCountries();
+  }, []);
+  console.log(countries);
+  const searchCountries = (text) => {
+    let matches = countries.filter((country)=>{
+      const regex = new RegExp(`${text}`  , "gi");
+      return country.name.match(regex) || country.capital.match(regex);
+    })
+    setCountryMatch(matches);
+  };
+  return (
+    <div className = "App">
+      <h2>Country Search</h2>
+      <Input
+        style = {{width : "40%" , marginTop:"10px"}}
+        placeholders = "Enter country name or capital name"
+        onChange = {(e) => searchCountries(e.target.value)}
+          />
+      {countryMatch && countryMatch.map((item , index)=>(
+      <div key = {index}>
+      <Card style = {{ width : "50% }} title = {`Country : ${item.name}`}>
+        Capital : {item.capital}
+        </Card>
+      </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
